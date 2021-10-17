db1908bd60440406aa604d54e5b1c8048afa9fd0
boarwell <34891695+boarwell@users.noreply.github.com>
2021-10-18 01:43:49 +0900

    update build script

--- build.js
+++ build.js
@@ -1,9 +1,12 @@
 import { build } from "esbuild";
 
+// ex. node ./build.js watch
+const watch = process.argv[2] === "watch";
+
 await build({
   entryPoints: ["./src/main.tsx"],
   outdir: "./build/",
   bundle: true,
-  watch: true,
-  minify: true
+  minify: true,
+  watch,
 });
